d54bd9021698ceff801c275a23ee0cf714dc62a1
shakogegia <shalva.gegia@gmail.com>
2021-01-08 22:05:48 +0100

    fix: self-invoking func

--- src/index.js
+++ src/index.js
@@ -2,48 +2,51 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const send = require("./send");
 
-try {
-  const url = core.getInput("url");
-  const status = core.getInput("status");
-  const title = core.getInput("title");
-  const description = core.getInput("description");
-  const mention = core.getInput("mention");
-  const mention_if = core.getInput("mention_if");
 
-  // github.context.job.status
-  console.log("🚀 ~ github.context", github.context)
-
-  const result = await send({
-    url,
-
-    content: `${github.workflow} - Success`,
-    embeds: [
-      {
-        title,
-        description,
-        fields: [
-          {
-            name: "Repository",
-            value: github.context.repository,
-          },
-          {
-            name: "Ref",
-            value: github.context.ref,
-          },
-          {
-            name: "Event Name",
-            value: github.context.eventName,
-          },
-          {
-            name: "Workflow",
-            value: github.context.workflow,
-          },
-        ],
-      },
-    ],
-  });
-
-  core.setOutput('result', result);
-} catch (error) {
-  core.setFailed(error.message);
-}
+(async () => {
+  try {
+    const url = core.getInput("url");
+    const status = core.getInput("status");
+    const title = core.getInput("title");
+    const description = core.getInput("description");
+    const mention = core.getInput("mention");
+    const mention_if = core.getInput("mention_if");
+  
+    // github.context.job.status
+    console.log("🚀 ~ github.context", github.context)
+  
+    const result = await send({
+      url,
+  
+      content: `${github.workflow} - Success`,
+      embeds: [
+        {
+          title,
+          description,
+          fields: [
+            {
+              name: "Repository",
+              value: github.context.repository,
+            },
+            {
+              name: "Ref",
+              value: github.context.ref,
+            },
+            {
+              name: "Event Name",
+              value: github.context.eventName,
+            },
+            {
+              name: "Workflow",
+              value: github.context.workflow,
+            },
+          ],
+        },
+      ],
+    });
+  
+    core.setOutput('result', result);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
